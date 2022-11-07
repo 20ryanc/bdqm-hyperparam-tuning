c50259c3207fe805164897b400607ed0d0cc1aca
schou35 <schou35@gatech.edu>
2022-11-07 02:59:12 -0500

    remove lab option from gpu-notebook

--- gpu-notebook.sh
+++ gpu-notebook.sh
@@ -4,5 +4,4 @@ pace-jupyter-notebook \
   -q pace-ice-gpu \
   -l nodes=1:ppn=1:gpus=1 \
   -l walltime=3:00:00 \
-  --conda-env=bdqm-hpopt \
-  --lab
+  --conda-env=bdqm-hpopt
--- setup-session.sh
+++ setup-session.sh
@@ -1,5 +1,5 @@
 # Convenience script for PACE jobs.
 # Run as `source setup-session.sh`
 
-module load anaconda3/2021.05
+module load anaconda3
 conda activate bdqm-hpopt
